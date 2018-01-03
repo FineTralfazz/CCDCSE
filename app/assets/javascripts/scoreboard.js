a9a51d4122c257486b9fada07af76f2157c8cc1a
Chris Bailey <chris@chrisbailey.io>
2018-01-02 20:55:18 -0900

    Only check for updates on scoreboard page

--- app/assets/javascripts/scoreboard.js
+++ app/assets/javascripts/scoreboard.js
@@ -1,11 +1,13 @@
 function check_updates() {
     $.getJSON('/scoreboard/last_check', function(data) {
-        if($('meta[name="last_check"]').attr('content') != data['last_check']) {
+        if ($('meta[name="last_check"]').attr('content') != data['last_check']) {
             window.location = window.location;
         }
     });
 }
 
 $(function() {
-    setInterval(check_updates, 1000);
+    if ($('meta[name="last_check"]').attr('content')) {
+        setInterval(check_updates, 1000);
+    }
 });
