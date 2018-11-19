8c04267da4aed5f0a6fc86f8528b0e7c9822d5cc
Chris Bailey <chris@chrisbailey.io>
2018-11-18 21:15:46 -0900

    Fix service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,10 +2,6 @@ class Service < ApplicationRecord
   has_many :checks
   after_initialize :init
 
-  def init
-    self.consecutive_fails  ||= 0
-  end
-
   def address(team)
     self.address_format.sub('?', team.number.to_s)
   end
